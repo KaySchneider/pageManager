7e6afcc6458e2d2c1e0ae5f15b11d29583b64b77
Kay Schneider <kayoliver82@gmail.com>
2012-01-08 12:45:51 +0100

    added an migration config helper

---
+++ config/migrateHelper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *this file contains some defines wich are for the
+ * migration time.. while not every class implements the
+ * config classes by itselfs 
+ */
+define('APP_API_KEY', AppInfo::appID());
+define('APP_SECRET', AppInfo::appSecret());
+define('APP_ID', APP_API_KEY);
+
+define('DB_USER', AppInfo::dataBaseUser());
+define('DB_HOST', AppInfo::dataBaseHost());
+define('DB_PASS', AppInfo::dataBasePass());
+define('DB_BASE', AppInfo::dataBaseBase());
+
+var_dump(DB_USER,DB_HOST,DB_PASS,DB_BASE);
+?>
