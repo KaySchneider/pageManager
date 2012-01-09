75fe2b12896241c1e2a5633534468ccf73cadec1
Kay Schneider <kayoliver82@gmail.com>
2012-01-09 15:18:59 +0100

    add js files

---
+++ controller/command_getSignedRequestCommand.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * Wenn nichts gesetzt ist dann hole die komplette �bersicht!
+ *
+ * Start Page Command sollte direkt mit der Datensammlung beginnen!
+*/
+
+/*
+ * die commands müssen benutzerfreundlicher werden!
+ * Es ist wünschenswert wenn ich eine Zentrale Stelle 
+ * habe an der ich mir die gewünschten Objekte holen kann
+ * und welche von der betreffenden Stelle mit der config 
+ * neu erstellt werden, mit Hilfe der Informationen aus der
+ * config Addresse.
+ * 
+ *  
+ * 
+ */
+
+class getSignedRequestCommand implements command {
+ 
+    public function execute(Request $request, Response $response) {
+        $req = registry::getInstance();
+        $req->setJSFiles('pageControl.js');
+        $db =  ATdb::get_instance();
+        
+
+        //get the facebook object and at it to the worker
+        $facebook = facebookFactory::getInstance();
+        $fbObject = $facebook->getFacebook();
+        $fbWorker = new FacebookOperation($fbObject);
+        $parsedRequest = $fbObject->getSignedReques();
+        echo json_encode($parsedRequest);
+        die();
+    }
+
+}
+?>
---
+++ js/pageControl.js
@@ -0,0 +1,138 @@
+var pageControl = function () {
+    (function (pageObj) {
+        userEnsureInitObj(pageObj,"init");    
+    })(this);
+    this.signedRequest = null;
+    console.log("buh");
+    //facebookHelper.registForLoginChange(this,'receiveLoginChange');
+    
+}
+
+/**
+ * extend the object facebookHelper from the snipFrameBaseMehtod
+ */
+pageControl.prototype = new baseMethod(['pageControlObj']);
+
+
+/**
+ * start the pageControl first when the facebook sdk is loaded
+ */
+pageControl.prototype.init = function () {
+    FB.Canvas.setSize({
+        width: 840, 
+        height: 480
+    });
+        console.log("buh");
+    this.getSignedRequest();
+    this.addEvents();
+}
+ 
+ 
+/**
+ * get the parsed signed_request object
+ * from the ajax backend
+ */
+pageControl.prototype.getSignedRequest = function () {
+    /**
+     * send the unparsed signed_request to the ajax backend
+     */
+    (function(pCObj){
+        var dataString = { 
+            mode:'getSignedRequest'
+         
+        };
+   
+        $.ajax({
+            type: "POST",
+            url: "index.php",
+            data: dataString,
+            dataType:'json',
+            success: function(data)
+            {
+                //set on success the parsed data out of the signed request
+                pCObj.signedRequest = data;
+            }
+        });
+    })(this);
+}
+
+/**
+ * getter for the parsed signed request object
+ */ 
+pageControl.prototype.getParsedRequest = function () {
+    if(this.signedRequest != null) {
+        return this.signedRequest;
+    } else {
+        return false;
+    }
+}
+
+pageControl.prototype.showError = function (errorArr) {
+   var max = errorArr.length;
+   //clean the error 
+   $('#error').html("");
+   
+   for(i=0;i<=max;i++) {
+       //print out the array in the document
+       var errorElement = document.createElement('p');
+       var inText = document.createTextNode(errorArr[i]);
+       errorElement.appendChild(inText);
+       $('#error').append(errorElement);
+   }
+}
+
+/**
+ *add some events to the page
+ */
+pageControl.prototype.addEvents = function () {
+    (function (eventObj) {
+       
+    })(this);
+   
+}
+function yes() {
+    //call again the init
+    pageControlObj.init();
+}
+
+function no() {
+    alert("you must login to the app to use it... sorry");
+}
+
+
+
+/**
+* if the all.js is loaded make the init
+*/
+window.fbAsyncInit = function() {
+    FB.init({
+        appId  : appId,
+        status : true, // check login status
+        cookie : true, // enable cookies to allow the server to access the session
+        xfbml  : true,  // parse XFBML
+        oauth : true
+    });
+
+    FB.Canvas.setAutoResize();
+    fbApiInit = true; //init flag
+    
+
+};
+
+/**
+ * make sure that the facebook init is done
+ * before maken an facebook call through the js 
+ * facebook sdk ( all.js ) 
+ */
+function fbEnsureInit(callback) {
+    if(!window.fbApiInit) {
+        setTimeout(function() {
+            fbEnsureInit(callback);
+        }, 50);
+    } else {
+        if(callback) {
+            callback();
+        }
+    }
+}
+        
