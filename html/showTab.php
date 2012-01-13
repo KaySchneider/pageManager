bd5ccd1023574732a3f2aae97b88d67f8133ac2d
Kay Schneider <kayoliver82@gmail.com>
2012-01-13 16:18:37 +0100

    added css

--- html/showTab.php
+++ html/showTab.php
@@ -2,20 +2,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml"
       xmlns:og="http://opengraphprotocol.org/schema/"
       xmlns:fb="http://www.facebook.com/2008/fbml" >
-<head>
-    <script src="js/jquery-1.6.min.js" language="javascript"></script>
-    <script src="js/tab.js" language="javascript"></script>
-    <link rel="stylesheet" type="text/css" href="css/tab.css"/>
-<body>
-    <div class="content">
-<?php
- 
-        print $this->vars['content'];
-
-?>
-   </div>
-   <div class="footer"
-        <span>powerd by <a target="_blank" href="https://apps.facebook.com/pagehelper/">pageManager</a>!</span>
-    </div>
-</body>
+    <head>
+        <script src="js/jquery-1.6.min.js" language="javascript"></script>
+        <script src="js/tab.js" language="javascript"></script>
+        <link rel="stylesheet" type="text/css" href="css/tab.css"/>
+    <body>
+        <div id="fb-root" ></div>
+        <div class="content">
+            <?php
+            print $this->vars['content'];
+            ?>
+        </div>
+        <div class="footer"
+             <span>powerd by <a target="_blank" href="https://apps.facebook.com/pagehelper/">pageManager</a>!</span>
+        </div>
+        <script type="text/javascript">
+              var appId = <?php echo APP_ID ?>;
+            (function() {
+                var e = document.createElement('script');
+                e.src = document.location.protocol + '//connect.facebook.net/<?php print USER_LOCALE; ?>/all.js'; //insert here the locale of the user, than the correct langauge of the all.js will be loaded
+                e.async = true;
+                document.getElementById('fb-root').appendChild(e);
+            }()); 
+        </script>
+    </body>
 </html>
--- js/tab.js
+++ js/tab.js
@@ -1,14 +1,16 @@
-$(document).ready(function () {
-   fbEnsureInitObj(resizeMe); 
+$('document').ready(function () {
+    fbEnsureInitObj(resizeMe); 
 });
 
 function resizeMe() {
-    FB.Canvas.setAutoResize();
+    console.log("setSize");
+    FB.Canvas.setSize();
 }
 /**
 * if the all.js is loaded make the init
 */
 window.fbAsyncInit = function() {
+    console.log("ju");
     FB.init({
         appId  : appId,
         status : true, // check login status
@@ -17,17 +19,23 @@ window.fbAsyncInit = function() {
         oauth : true
     });
     fbApiInit = true; //init flag
-    FB.Canvas.setAutoResize();
+    FB.Canvas.setSize();
 };
 
 function fbEnsureInitObj( method) {
     if(!window.fbApiInit) {
-       setTimeout(function() {
+        setTimeout(function() {
             fbEnsureInitObj(method);
         }, 50);
     } else {
+        console.log(method);
         if(method) {
             method();
         }
     }
 }
+
+
+/**
+             * load the all.js from the javscript facebook sdk asynchronusly
+             */
